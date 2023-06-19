db1904731ed7b28b96232d643221db2aec64a7f1
Amer-On <a20051772@yandex.ru>
2023-06-19 22:57:58 +0300

    moved app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+import "./App.css"
+
+function App() {
+    return (
+        <div className='main-container'>
+            <Header/>
+            <p> Map</p>
+        </div>
+
+    );
+}
+
+export default App;
