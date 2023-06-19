7befe0242e47063bee2185eb6382500930eca39a
Amer-On <a20051772@yandex.ru>
2023-06-20 00:00:59 +0300

    wip with svg map

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import SearchBar from "./building-map/SearchBar";
 import "./App.css"
+import Map from "./building-map/Map"
 
 function App() {
     return (
-        <>
+        <div className='main-container'>
+            <Map/>
             <SearchBar/>
-            <p> Map</p>
-        </>
+        </div>
     );
 }
 
